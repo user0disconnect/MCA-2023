cbacef90df822d2a9dc3874017fcd51e5ed5e2cd
anastasia <109434678+user0disconnect@users.noreply.github.com>
2023-11-19 13:27:14 +0200

    Delete Labs/week1/popuot.js

--- Labs/week1/popuot.js
+++
@@ -1,8 +0,0 @@
-var button = document.querySelector(".popup"),
-  items = document.querySelectorAll(".trigger");
-var openCloseMenu = function () {
-  for (i = 0; i < items.length; i++) {
-    items[i].classList.toggle("slideout");
-  }
-};
-button.onclick = openCloseMenu;
